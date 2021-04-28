4a1fa4a463fcd1ae1b7a13f2579a49b0b5690e9d
David La Grange <davidnlagrange@hotmail.com>
2021-04-28 10:39:53 -0500

    Completed Models + Created and Registered App URLS + Temp Index View Function Implementation

--- movie_library_project/movies_app/admin.py
+++ movie_library_project/movies_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Movie, MovieReview, MovieRating
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(MovieReview)
+admin.site.register(MovieRating)
--- movie_library_project/movies_app/models.py
+++ movie_library_project/movies_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+    runtime = models.IntegerField()
+    release_date = models.DateField()
+    likes = models.IntegerField()
+    watch_count = models.IntegerField()
+
+class MovieReview(models.Model):
+    text = models.CharField(max_length=200)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+class MovieRating(models.Model):
+    rating = models.IntegerField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
